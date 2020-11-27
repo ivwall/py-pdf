5c4651edc6aa6e4fc6dcfda589c079e8a07cb5c9
Ian <ivwall@tgndomains.com>
2020-11-27 08:48:49 -0500

    new dir to start developing csv parser

---
+++ 002-parse-csv/CSVParser.py
@@ -0,0 +1,6 @@
+class CSVParser:
+	def f(self):
+		return 'parse a file'
+
+	def p():
+		print("parse a csv file class call CSVParser")
---
+++ 002-parse-csv/pcsv.py
@@ -0,0 +1,11 @@
+from CSVParser import CSVParser
+
+csvParser = CSVParser 
+
+def main():
+	csvParser.p()
+	print("parse csv file, but pase in a class")
+
+
+if __name__ == "__main__":
+	main()
